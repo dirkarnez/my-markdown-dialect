7409aa14bb9bc83a9083c5d3725ee2ddace6709e
dirkarnez <smalldirkalex@gmail.com>
2026-02-22 19:00:40 +0800

    - wip

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	// read md file and generate
 	o, _ := markup.Parse("This sentence uses `$` delimiters to show math inline: $\\sqrt{3x-1}+(1+x)^2$<hr>```123```")
 	fmt.Println(markup.Finalize(o))
 }
--- markup/template.go
+++ markup/template.go
@@ -72,6 +72,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
 });
 </script> 
 
+<script>
+(function () {
+  const body = document.body.textContent;
+  if (body.match(/(?:\$|\\\(|\\\[|\\begin\{.*?})/)) {
+    if (!window.MathJax) {
+      window.MathJax = {
+        tex: {
+          inlineMath: {'[+]': [['$', '$']]}
+        }
+      };
+    }
+    const script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@4/tex-chtml.js';
+    document.head.appendChild(script);
+  }
+})();
+</script>
 </body>
 </html>
 `)
